53ece45f8f2823f680345e7028235a46045f0234
Ren-De-Yang <duke930323@gmail.com>
2023-04-27 10:10:38 +0800

    define shape, color, block

--- ProjectTetris/main.c
+++ ProjectTetris/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <windows.h>
+
+typedef enum {
+	RED = 41,
+	GREEN,
+	YELLOW,
+	BLUE,
+	PURPLE,
+	CYCN,
+	WHITE,
+	BLACK = 0
+} Color;
+
+typedef enum {
+	EMPTY = -1,
+	I,
+	J,
+	L,
+	O,
+	S,
+	T,
+	Z
+} ShapeId;
+
+typedef struct {
+	Color color;
+	ShapeId shape;
+} Block;
 
 int main()
 {
+	SetConsoleOutputCP(CP_UTF8);
 	printf("hello world");
 
 	return 0;
